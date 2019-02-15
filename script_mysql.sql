5a8c04c095e413a48df3a2db16db801e40118ddb
Alex Rocha <alrocha003@gmail.com>
2019-02-15 00:00:06 -0200

    Upload script to create tables

---
+++ script_mysql.sql
@@ -0,0 +1,13 @@
+create database teste
+
+use teste
+
+
+CREATE TABLE animals (
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     name CHAR(30) NOT NULL,
+     idade integer,
+     createdAt DateTime,
+     updatedAt DateTime,
+     PRIMARY KEY (id)
+);
