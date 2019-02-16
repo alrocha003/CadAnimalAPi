2c9225b58d4155caffd511cbf3af9b7bc3e19dbf
Alex Rocha <alrocha003@gmail.com>
2019-02-16 11:40:31 -0200

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,9 @@ const sequelize = require('./database/database');
 const bodyParser = require('body-parser');
 const app = express();
 const hostname = "127.0.0.1";
-const port = "3000";
+const port = "3010";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("port", port);
 app.use('/api', animalsRoute);
 app.use((req, res) => {
@@ -18,6 +20,6 @@ app.use(bodyParser.urlencoded({
 }));
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
-    console.log('servidor rodando na porta 3000');
+    console.log('servidor rodando na porta 3010');
     sequelize.sync();
 });
